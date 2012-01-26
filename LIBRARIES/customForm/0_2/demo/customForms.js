e861bc7ee96bd0974630436318b4a02d75aabf8f
Mitermayer Reis <mitermayer.reis@gmail.com>
2012-01-26 19:50:24 +1100

    refactoring code

--- LIBRARIES/customForm/0_2/demo/customForms.js
+++ LIBRARIES/customForm/0_2/demo/customForms.js
@@ -34,237 +34,241 @@ THE SOFTWARE.
   // $('form').cstmForm();
 
 */
-
 (function( $ ){
-  "use strict";
-  $.fn.cstmForm = function( options ) {
+  
+  $.fn.cstmForm = function( options ) 
+  {
         
-    var selector = this; // forms that will be affected, can be an array or a single object    
-    var settings = {
+    var $arr = this, // forms that will be affected, can be an array or a single object   
+
+    defaults = 
+    {
     
         // box      :  the element that will be created as the custom replacement element.
         // wrapper  :  wrapper for the select box
         // forms    :  pass id of the object as #id or it will search for all forms
-        'box'         : 'a',
-        'wrappper'    : 'div',
-        'prefix'      : 'custom-'
-    }; 
-    
-    var checkbox = function(arr) {
+        box            : 'a',
+        wrappper       : 'div',
+        prefix         : 'custom-'
+    },
+
+    core = 
+    {
+        generate_id: function( $elem )
+        {
+            return defaults.prefix + ( $elem.attr('name') || $elem.attr('id') ); // generate id based on name or id of the elment
+        },
+        hide_element: function( $elem, transparent )
+        {
+            // If transparent just hide with opacity else hide with normal display none
+            if ( transparent )
+            {
+                $elem.css({ 
+                     'opacity': '0',
+                     'filter': 'alpha(opacity=0)',
+                     '-moz-opacity': '0',
+                     '-khtml-opacity': '0'
+                }); 
+            }
+            else 
+            {
+                $(this).hide();
+            }
+        },
+        sort_elements: function( $arr )
+        {
+            var c=[], r=[], f=[], s=[];
+
+            $($arr).each( function() 
+            {                
+                if ($(this).is('input[type=checkbox]')) {
+                    c.push($(this));
+                } else if ($(this).is('input[type=radio]')) {
+                    r.push($(this));
+                } else if ($(this).is('input[type=file]')) {
+                    f.push($(this));
+                } else if ($(this).is('select')) {
+                    s.push($(this));
+                }                
+            });   
+
+            return { checkbox: c, radio: r, file: f, select: s };
+        },
+        load_modules: function( elems )
+        {            
+            if ( elems.checkbox.length ) {
+                    checkbox( elems.checkbox );
+            }
+            if ( elems.select.length ) {
+                    select( elems.select );
+            }
+            if ( elems.file.length ) {
+                    file( elems.file );               
+            }
+            if ( elems.radio.length ) {
+                    radio( elems.radio );               
+            }
+        }
+    },
+        
+    checkbox = function(arr) 
+    {
     // creates an element eg : <span id="custom-email" class="customForm-checkbox" >Email</span>
 
         $(arr).each( function() {  
 
-          var currentElement = this; // creates a reference to this element          
-          var newId = settings.prefix + ( $(currentElement).attr('name') || $(currentElement).attr('id') );  // generate id based on name or id of the elment
+          var $curEle = this, // creates a reference to this element          
+              newId = core.generate_id( $curEle );
            
           // generate the custom new elements before the element
-          $(this).before( $( "<" + settings.box +"/>", { 
+          $curEle.before( $( "<" + defaults.box +"/>", { 
                 id: newId, 
-                "class": settings.prefix + "checkbox", 
+                "class": defaults.prefix + "checkbox", 
                 click: function(e) {
-                    e.preventDefault(); // in case of settings.box as an a tag, prevent it to procced
+                    e.preventDefault(); // in case of defaults.box as an a tag, prevent it to procced
                     
                     $(this).toggleClass('checked'); 
                     
                     if ( $(this).hasClass('checked') ) {
-                        $(currentElement).prop('checked', true);
+                        $curEle.prop('checked', true);
                     } else {
-                        $(currentElement).prop('checked', false);
+                        $curEle.prop('checked', false);
                     }
                 }
           }));
 
           // in case a form was submitted to save the state of the currently checked button
-          if ( $(currentElement).prop('checked') ) {
+          if ( $curEle.prop('checked') ) {
             var newElementId = '#' + newId;
             $(newElementId).addClass('checked');
           }
           
           // hides this element
-          $(this).addClass('customForm-hidden');
+          $curEle.addClass('customForm-hidden');
         });
-    };
+    },
     
-    var radio = function(arr) {
+    radio = function(arr) 
+    {
     
           var radios = arr; // create a reference to radios
           
           $(radios).each( function() { 
-              var currentElement = this, // creates a reference to this element          
-                  newId = settings.prefix + ( $(currentElement).attr('id') || $(currentElement).attr('name') ) + "-" + $(currentElement).attr('value');  // generate id based on name or id of the elment
+              var $curEle = this, // creates a reference to this element          
+                  newId = core.generate_id( $curEle );
                             
               // generate the custom new elements before the element
-              $(this).before( $( "<" + settings.box +"/>", { 
+              $curEle.before( $( "<" + defaults.box +"/>", { 
                     id: newId, 
-                    "class": settings.prefix + "radio", 
+                    "class": defaults.prefix + "radio", 
                     click: function(e) {
-                        e.preventDefault(); // in case of settings.box as an a tag, prevent it to procced
+                        e.preventDefault(); // in case of defaults.box as an a tag, prevent it to procced
                         
-                        $("." + settings.prefix + "radio").removeClass('checked');
+                        $("." + defaults.prefix + "radio").removeClass('checked');
                         $(this).addClass('checked');
                         
                         // loop all and remove the 
                         $(radios).each( function() { 
-                            $(this).attr('checked', '');
+                            $(this).prop('checked', false);
                         });                       
-                        $(currentElement).attr('checked', 'checked');
+                        $curEle.prop('checked', true);
                     }
               }));
               
-              $('#' + newId).addClass(settings.prefix + $(currentElement).attr('name'));
+              $('#' + newId).addClass(defaults.prefix + $curEle.attr('name'));
             
               // in case a form was submitted to save the state of the currently checked button
-              if ( $(currentElement).attr('checked') ) {
+              if ( $curEle.attr('checked') ) {
                 $('#' + newId).addClass('checked');                
               }
               
               // hides this element
-              $(this).addClass('customForm-hidden');
+              $curEle.addClass('customForm-hidden');
          });
-    };
+    },
     
-    var file = function(arr) {        
+    file = function(arr) {        
         
         $(arr).each( function() { 
         
-            var currentElement = this,
-                newId = settings.prefix + ( $(currentElement).attr('id') || $(currentElement).attr('name') ),  // generate id based on name or id of the element
+            var $curEle = this,
+                newId = core.generate_id( $curEle ),
                 containerId = '#' + newId + "-container",
                 fileId = '#' + newId;
             
-            $(this).css({ 
-                 'opacity': '0',
-                 'filter': 'alpha(opacity=0)',
-                 '-moz-opacity': '0',
-                 '-khtml-opacity': '0'
-            }); 
+            core.hide_element( $(this), true );
             
-            $(this).before( $( "<" + settings.wrappper +"/>", { 
+            $curEle.before( $( "<" + defaults.wrappper +"/>", { 
                     id: newId + "-container"
             }));
            
             $(containerId).css('position', 'relative');
            
-            $(currentElement).appendTo(containerId);
+            $curEle.appendTo(containerId);
             
-            $("<" + settings.box +"/>", {
-              "class": settings.prefix + "file", 
+            $("<" + defaults.box +"/>", {
+              "class": defaults.prefix + "file", 
               id: newId
             }).appendTo(containerId);
             
             $(fileId).html($('input[type=file]').val().split('\\').pop()); // this is where the text will be done
             
-            $(this).css('position', 'absolute');
-            $(this).css('top', '0px');
-            $(this).css('left', '0px');
+            $curEle.css('position', 'absolute');
+            $curEle.css('top', '0px');
+            $curEle.css('left', '0px');
             
-            $(this).change(function() {
+            $curEle.change(function() {
                 // this is the event to update file text
                 $(fileId).html($('input[type=file]').val().split('\\').pop());
             });
         });
-    };
+    },
 
-    var select = function(arr) {        
+    select = function(arr) {        
         
         $(arr).each( function() { 
         
-            var currentElement = this,
-                newId = settings.prefix + ( $(currentElement).attr('id') || $(currentElement).attr('name') ),  // generate id based on name or id of the element
+            var $curEle = this,
+                newId = core.generate_id( $curEle ),
                 containerId = '#' + newId + "-container",
                 selctId = '#' + newId;
             
-            // hides the select box
-            $(this).css({ 
-                 'opacity': '0',
-                 'filter': 'alpha(opacity=0)',
-                 '-moz-opacity': '0',
-                 '-khtml-opacity': '0'
-            }); 
+            core.hide_element( $(this), true );
             
-            $(this).before( $( "<" + settings.wrappper +"/>", { 
+            $curEle.before( $( "<" + defaults.wrappper +"/>", { 
                     id: newId + "-container"
             }));
            
             $(containerId).css('position', 'relative');
            
-            $(currentElement).appendTo(containerId); // moves the selectbox to this container
+            $curEle.appendTo(containerId); // moves the selectbox to this container
             
-            $("<" + settings.box +"/>", {
-              "class": settings.prefix + "select", 
+            $("<" + defaults.box +"/>", {
+              "class": defaults.prefix + "select", 
               id: newId
             }).appendTo(containerId);
             
             $(selctId).html($(containerId + " option:selected").text());
             
-            $(this).css('position', 'absolute');
-            $(this).css('top', '0px');
-            $(this).css('left', '0px');
+            $curEle.css('position', 'absolute');
+            $curEle.css('top', '0px');
+            $curEle.css('left', '0px');
             
-            $(this).change(function() {
+            $curEle.change(function() {
                 $(selctId).html($(containerId + " option:selected").text());
             });
         });
     };
     
-    var load_module = {
-        'checkbox' : function(arr) { checkbox(arr); },
-        'radio'    : function(arr) { radio(arr); },
-        'file'     : function(arr) { file(arr); },
-        'select'   : function(arr) { select(arr); }
-    };
-    
-    var init = function( options, selector, module ) {
-        
+    (function( options, $arr ) {        
         // check to for object, if it exists start the pluggin, else return
-        if ( selector.length ) {
-
-            if ( options ) {
-               $.extend( settings, options ); // if options are passed, overlap the settings with the new options
-            }
-            
-            // the elements will be stored in here
-            var checkbox = [],
-                radio = [],
-                file = [],
-                select = [];
-            
-            if (selector.is('form')) {
-                selector = $('form').find(":input");
-            }            
-           
-            $(selector).each( function(index) {                
-                if ($(this).is('input[type=checkbox]')) {
-                    checkbox.push($(this));
-                } else if ($(this).is('input[type=radio]')) {
-                    radio.push($(this));
-                } else if ($(this).is('input[type=file]')) {
-                    file.push($(this));
-                } else if ($(this).is('select')) {
-                    select.push($(this));
-                }                
-            });   
-           
-            
-            if ( checkbox.length ) {
-                    load_module.checkbox( checkbox );
-            }
-            if ( select.length ) {
-                    load_module.select( select );
-            }
-            if ( file.length ) {
-                    load_module.file( file );               
-            }
-            if ( radio.length ) {
-                    load_module.radio( radio );               
-            }
-            
-                   
-        } else {
-            return false;
+        if ( $arr.length ) 
+        {
+            defaults = options ? $.extend({}, defaults, options) : defaults;
+            $arr = $arr.is('form') ? $('form').find(":input") : $arr;                   
+            core.load_modules( core.sort_elements( $arr ) );                   
         }
-    }( options, selector, load_module );
+    }( options, $arr ));
 
   };
 })( jQuery );
